d4f2424d3b1399d28c94523161ae6639524146c6
0918nobita <nobita.0918@gmail.com>
2021-06-13 19:12:04 +0900

    Insert rows into `tournament` table

--- mysql/initdb/init.sql
+++ mysql/initdb/init.sql
@@ -11,3 +11,31 @@ CREATE TABLE tournament (
     holding_period_from     DATETIME NOT NULL,
     holding_period_to       DATETIME NOT NULL
 ) DEFAULT CHARACTER SET=utf8;
+
+INSERT INTO tournament(
+    title,
+    application_period_from,
+    application_period_to,
+    holding_period_from,
+    holding_period_to
+) VALUES (
+    "APEX CR Cup",
+    "2021-06-01 15:00:00",
+    "2021-06-10 23:59:00",
+    "2021-06-12 19:00:00",
+    "2021-06-12 22:00:00"
+);
+
+INSERT INTO tournament(
+    title,
+    application_period_from,
+    application_period_to,
+    holding_period_from,
+    holding_period_to
+) VALUES (
+    "APEX RG FES",
+    "2021-06-13 18:00:00",
+    "2021-06-13 20:00:00",
+    "2021-06-01 17:00:00",
+    "2021-06-10 23:59:00"
+);
