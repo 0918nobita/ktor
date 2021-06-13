ae04f37453288c937aff25144122fe50fc9d360a
0918nobita <nobita.0918@gmail.com>
2021-06-13 15:47:35 +0900

    /v1/tournament/{id} にリクエストされた時の処理を追加した

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.5.0"
+    kotlin("plugin.serialization") version "1.5.0"
 }
 
 group = "net.resports"
@@ -14,9 +15,13 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion = "1.6.0"
+val logbackVersion = "1.2.3"
+
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-server-core:1.6.0")
-    implementation("io.ktor:ktor-server-netty:1.6.0")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,52 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.*
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import java.time.ZonedDateTime
+
+@Serializable
+@SerialName("Period")
+data class PeriodSurrogate(val from: String, val to: String)
+
+object PeriodSerializer : KSerializer<Period> {
+    override val descriptor = Period.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: Period) {
+        val surrogate = PeriodSurrogate(from = value.from.toString(), to = value.to.toString())
+        encoder.encodeSerializableValue(PeriodSurrogate.serializer(), surrogate)
+    }
+
+    override fun deserialize(decoder: Decoder): Period {
+        val surrogate = decoder.decodeSerializableValue(PeriodSurrogate.serializer())
+        return Period(from = ZonedDateTime.parse(surrogate.from), to = ZonedDateTime.parse(surrogate.to))
+    }
+}
+
+@Serializable(with = PeriodSerializer::class)
+data class Period(val from: ZonedDateTime, val to: ZonedDateTime)
+
+@Serializable
+data class Tournament(val title: String, val holdingPeriod: Period, val applicationPeriod: Period)
 
 fun main() {
-    embeddedServer(Netty, port = 8000) {
+    val tournament = Tournament(
+        title = "APEX CRカップ",
+        holdingPeriod = Period(
+            ZonedDateTime.parse("2021-06-12T19:00:00+09:00"),
+            ZonedDateTime.parse("2021-06-12T22:00:00+09:00")
+        ),
+        applicationPeriod = Period(
+            ZonedDateTime.parse("2021-06-01T15:00:00+09:00"),
+            ZonedDateTime.parse("2021-06-10T23:59:00+09:00")
+        )
+    )
+    embeddedServer(Netty, port = 1234) {
         routing {
-            get("/") {
-                call.respondText("Hello, world!")
+            get("/v1/tournament/{id}") {
+                call.respond(Json.encodeToString(tournament))
             }
         }
     }.start(wait = true)
